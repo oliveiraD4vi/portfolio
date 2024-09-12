059f0889a0e7ee0d0b23e1fc99251a1a787b3a80
Davi Oliveira <davi.comp.oliveira@gmail.com>
2024-09-12 14:23:53 -0300

    feat: added home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'Davi Oliveira',
+    pathMatch: 'full',
+    component: HomeComponent
+  },
+  {
+    path: '**',
+    redirectTo: '/',
+  }
+];
