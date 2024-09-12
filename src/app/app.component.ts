0233e1a455e7ab01868fd13252b7f53eca6ebe28
Davi Oliveira <davi.comp.oliveira@gmail.com>
2024-09-12 16:56:03 -0300

    feat: added basic header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { SharedModule } from './shared/shared.module';
+import { HeaderComponent } from "./components/layout/header/header.component";
+import { FooterComponent } from "./components/layout/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [SharedModule],
+  imports: [SharedModule, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
